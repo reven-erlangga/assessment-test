fd34cf0f3ffd0ddb267fd272176ed517a2bbeadf
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2025-01-15 10:01:41 +0700

    change cron expression

--- BE/src/tickets/cron/cron.service.ts
+++ BE/src/tickets/cron/cron.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class CronService {
-    private readonly timezone: string;
-
     constructor(
         private readonly prisma: PrismaService,
-        private readonly configService: ConfigService,
-    ) {
-        this.timezone = this.configService.get<string>('TIMEZONE');
-    }
+    ) { }
 
-    @Cron(CronExpression.EVERY_MINUTE)
+    @Cron(CronExpression.EVERY_HOUR)
     async handleCron() {
         const now = new Date();
 
@@ -27,7 +21,6 @@ export class CronService {
             data: { priority: 'URGENT' },
         });
 
-        // Update Urgent tickets to Emergency after 6 hours
         const urgentThreshold = new Date(now.getTime() - 6 * 60 * 60 * 1000); // 6 hours
         await this.prisma.ticket.updateMany({
             where: {
